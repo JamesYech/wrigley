19599bb5419a74e55f6eed81797fbcc8fac944a5
JamesYech <jyech@jdtech.net>
2019-01-30 19:59:45 -0500

    moved map load to own component

--- src/components/App.js
+++ src/components/App.js
@@ -1,51 +1,14 @@
 import React, { Component } from 'react';
 import mapStyles from '../utils/mapStyle.js'
-
+// import GetMarkers from './markers.js'
 import '../css/App.css'
+import Map from './Map.js'
 
-
-
+const center = {lat: 41.9484384, lng:-87.6575214}
 
 
 class App extends Component {
 
-
-  constructor(props) {
-	super(props)
-	  this.center ={lat: 41.9484384, lng:-87.6575214}
-	  this.state = {
-		mapIsReady: false,
-		wrigleyMap: {},
-	  }
-  }
-
-
-  componentDidMount() {
-
-  	// have to catch when google not available
-
-	const ApiKey = 'AIzaSyDnImGieQyxOm3Ywe48ygJGQXMHm2JkpCY'
-	const script = document.createElement('script')
-	script.src = `https://maps.googleapis.com/maps/api/js?key=${ApiKey}&libraries=places`
-	script.async = true
-	script.defer = true
-	script.addEventListener('load', () => {
-	  this.setState({mapIsReady: true})
-	})
-	document.body.appendChild(script)
-  }
-
-
-	componentDidUpdate() {
-		if(this.state.mapIsReady) {
-			let map = new window.google.maps.Map(document.getElementById('map'), {
-				center: {lat: 41.9484384, lng:-87.6575214},
-				zoom: 17,
-				styles: mapStyles.mapStyles,
-				mapTypeControl: false
-			})
-		}
-	}
 
 
 	render() {
@@ -58,8 +21,17 @@ class App extends Component {
 				<div className='grid'>
 					<div id='placesPanel' className='col-1 placesPanel'>
 						Search & Display Panel
+
 					</div>
-					<div id='map' className='col-3 mapPanel' />
+					<Map
+						id="map"
+						options ={{
+							center: center,
+							zoom: 15,
+							styles: mapStyles.mapStyles,
+							mapTypeControl: false
+						}}
+					/>
 				</div>
 
 
